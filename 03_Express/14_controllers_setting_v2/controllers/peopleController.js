958734ee1f20aaeced391bc7cec2d5e7b78b1836
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-24 13:22:52 +0200

    formatting files

--- 03_Express/14_controllers_setting_v2/controllers/peopleController.js
+++ 03_Express/14_controllers_setting_v2/controllers/peopleController.js
@@ -1,53 +1,62 @@
-
-const { people } = require('./../data');
+const { people } = require("./../data");
 
 //---------------------------------------------------------------------
 exports.getPeople = (req, res) => {
   res.status(200).json({ success: true, people });
-}
+};
 
 //---------------------------------------------------------------------
 exports.createPeople = (req, res) => {
   const { name } = req.body;
   if (!name) {
-    return res.status(400).json({ success: false, msg: 'please provide name value' });
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
   }
   res.status(201).json({ success: true, name });
-}
+};
 
 //---------------------------------------------------------------------
 exports.createPeoplePostman = (req, res) => {
   const { name } = req.body;
   if (!name) {
-    return res.status(400).json({ success: false, msg: 'please provide name value' });
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
   }
   res.status(201).json({ success: true, data: [...people, name] });
-}
+};
 
 //---------------------------------------------------------------------
 exports.updatePeople = (req, res) => {
   const person = people.find((person) => person.id === Number(req.params.id));
   if (!person) {
-    return res.status(404).json({ success: false, msg: `no person with id ${id}`});
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
   }
   const { name } = req.body;
   if (!name) {
-    return res.status(400).json({ success: false, msg: `Please, provide a content to be updated`});
+    return res
+      .status(400)
+      .json({ success: false, msg: `Please, provide a content to be updated` });
   }
   person.name = name;
-  res.status(200).json({ success: true, data: people});
-}
+  res.status(200).json({ success: true, data: people });
+};
 
 //---------------------------------------------------------------------
 exports.deletePeople = (req, res) => {
   const person = people.find((person) => person.id === Number(req.params.id));
   if (!person) {
-    return res.status(404).json({ success: false, msg: `no person with id ${req.params.id}`});
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
   }
   const newPeople = people.filter(
     (person) => person.id !== Number(req.params.id)
-    )
-    return res.status(200).json({ success: true, data: newPeople });
-  }
-  
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+};
+
 //---------------------------------------------------------------------
